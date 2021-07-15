18f8bae9a603deaa119d016b757c66739f63e714
username github <email github>
2021-07-15 17:53:02 +0700

    pha ke 1-1

--- b7/js/index.js
+++ b7/js/index.js
@@ -40,7 +40,27 @@ form.addEventListener("submit", (e) =>{
         const usernameError = document.getElementById('username-error')
         usernameError.innerHTML = ""
         usernameError.style = "display: none"
-    }
+    }    
+
+    if(data.username.length < 4) {
+        const shorterError = document.getElementById('shorter-error')
+        shorterError.innerHTML = "Username cannot shorter than 4 letters"
+        shorterError.style = "display: block"
+      } else {
+        const shorterError = document.getElementById('shorter-error')
+        shorterError.innerHTML = ""
+        shorterError.style = "display: none"
+      }
+
+      if(data.username.length > 16) {
+        const longerError = document.getElementById('longer-error')
+        longerError.innerHTML = "Password cannot longer than 16 letters"
+        longerError.style = "display: block"
+      } else {
+        const longerError = document.getElementById('longer-error')
+        longerError.innerHTML = ""
+        longerError.style = "display: none"
+      }
 
     if(data.password === ""){
         const passwordError = document.getElementById('password-error')
@@ -53,6 +73,26 @@ form.addEventListener("submit", (e) =>{
         passwordError.style = "display: none"
     }
 
+    if(data.password.length < 8) {
+        const shortError = document.getElementById('short-error')
+        shortError.innerHTML = "Password cannot shorter than 8 letters"
+        shortError.style = "display: block"
+      } else {
+        const shortError = document.getElementById('short-error')
+        shortError.innerHTML = ""
+        shortError.style = "display: none"
+      }
+
+      if(data.password.length > 16) {
+        const longError = document.getElementById('long-error')
+        longError.innerHTML = "Password cannot longer than 16 letters"
+        longError.style = "display: block"
+      } else {
+        const longError = document.getElementById('long-error')
+        longError.innerHTML = ""
+        longError.style = "display: none"
+      }
+     
     if(data.age === "no_value"){
         const ageError = document.getElementById('age-error')
         ageError.innerHTML = "Age cannot be empty"
@@ -67,3 +107,8 @@ form.addEventListener("submit", (e) =>{
 
     console.log(data);
 })
+
+
+
+
+
